4092b6f80d8df14060e0c3dbc04fad6664207a46
AlejoRam05 <emmanuel.alej15@gmail.com>
2024-10-13 00:21:55 -0300

    Cambios realizados para el correcto funcionamiento

--- User/models/user_model.py
+++ User/models/user_model.py
@@ -1,24 +1,35 @@
 """Definimos la entidad USER"""
 from pydantic import EmailStr
 from datetime import datetime
+from typing import Optional, Annotated
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from sqlalchemy import text
 from dotenv import load_dotenv
-import os
+import os 
+from fastapi import Depends, HTTPException, Query
+
 # from dotenv import load_dotenv
 #BaseModel nos ayuda a crear nuestro modelo
+load_dotenv()
+sqlite_file_name = os.getenv("sqlite_file_name")
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 class UserMain(SQLModel, table=True):
     "Valores/Data que debe ingresar el usuario, valores que espera la Clase User"
-    id: int | None = Field(default=None, primary_key= True)
+    id: int = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     username: str = Field(index=True)
     email: EmailStr 
     hashed_password: str 
     created_at: datetime = Field(default_factory=datetime.now)
-    updated_at: datetime | None = None
-    
-# name: str | None = None  "| None = None -> nos permite obviar ese parametro"
+    updated_at: Optional[datetime] = Field(default=None)
 
 class DatabaseUser:
     
@@ -36,3 +47,24 @@ class DatabaseUser:
                 print("Tablas creadas:", tables)
         except Exception as e:
             print("Error al interactuar con el Database", e)
+
+
+async def create_cliente(cliente: UserMain, session: SessionDep) -> UserMain:
+    try:
+        session.add(cliente)
+        session.commit()
+        session.refresh(cliente)
+        return cliente
+    except Exception as e:
+        session.rollback()  # Deshacer cambios en caso de error
+        raise HTTPException(status_code=400, detail=f"Error al crear el usuario: {str(e)}")
+    
+async def read_clientes(session: SessionDep, offset: int=0, limit: Annotated[int, Query(le=100)]=100) -> list[UserMain]:
+    statement = select(UserMain).offset(offset).limit(limit)
+    return session.exec(statement).all()
+
+def read_usuario(id: int, session: SessionDep) -> UserMain:
+    usuario = session.get(UserMain, id)
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return usuario
