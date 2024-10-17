cb5135cd40d2919b2bfce70629fc2b1fd2a5d878
AlejoRam05 <165935543+AlejoRam05@users.noreply.github.com>
2024-10-17 20:44:31 -0300

    schemas para el cuerpo del microserver inventario

---
+++ inventario/schemas/inv_schemas.py
@@ -0,0 +1,23 @@
+"""MODELO DE QUE DATOS ESTARIA ESPERANDO PARA PROCESAR """
+from datetime import datetime
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+class InventoryBase(BaseModel):
+    
+    product: str = Field(index = True)
+    price: int 
+    description: str
+    stock: int
+    
+class InventoryDB(SQLModel, table = True):
+    
+    code: Optional[int] = Field(index=True, primary_key=True, default=None)
+    product: str 
+    price: int 
+    description: str
+    stock: int
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: Optional[datetime] = Field(default=None)
+    
